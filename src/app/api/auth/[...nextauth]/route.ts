b012d43f27d6e6099cd1f912498187349104b2d1
Zeit241 <easywin.ru@gmail.com>
2023-10-27 20:15:41 +0300

    Обновлен файл авторизации

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,30 +1,52 @@
-import { db } from "@/lib/db";
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import { DefaultSession, NextAuthOptions } from "next-auth";
-import NextAuth from "next-auth/next";
-import { env } from "@/lib/env.mjs"
+import {UserSession} from "@/../types/custom-types"
+import {PrismaAdapter} from "@auth/prisma-adapter"
+import {NextAuthOptions} from "next-auth"
+import NextAuth from "next-auth/next"
+import bcrypt from "bcrypt";
+import CredentialProvider from "next-auth/providers/credentials"
+import {db} from "@/lib/db"
 
-
-declare module "next-auth" {
-  interface Session {
-    user: DefaultSession["user"] & {
-      id: string;
-    };
-  }
-}
 
 export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(db),
-  callbacks: {
-    session: ({ session, user }) => {
-      session.user.id = user.id;
-      return session;
+    adapter: PrismaAdapter(db),
+    session: {
+        strategy: "jwt",
+        maxAge: 7 * 24 * 60 * 60, // 30 days
     },
-  },
-  providers: [
-     
-  ],
-};
+    callbacks: {
+        jwt: async ({token, user}) => {
+            user ? (token.user = <UserSession>user) : null
+            return token
+        },
+        session: async ({session, token}) => {
+            session.user = token.user
+            return session
+        },
+    },
+    pages: {
+        signIn: "/signin",
+    },
+    providers: [
+        CredentialProvider({
+            name: "credentials",
+            credentials: {
+                username: {label: "Username", type: "text"},
+                password: {label: "Password", type: "password"},
+            },
+            async authorize(credentials, req) {
+                const candidate = await db.user.findUnique({
+                    where: {
+                        username: credentials?.username,
+                    },
+                })
+                if (candidate && bcrypt.compareSync(credentials?.password!, candidate.password)) {
+                    return candidate
+                }
+                return null
+            },
+        }),
+    ],
+}
 
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
+const handler = NextAuth(authOptions)
+export {handler as GET, handler as POST}
