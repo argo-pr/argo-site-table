0bc52ba8da9a2f3bd5cc4bb5eb02edce4ac50e8f
Zeit241 <easywin.ru@gmail.com>
2023-10-27 20:29:59 +0300

    Незначительные обновления

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,71 +1,73 @@
-import { getUserAuth } from "@/lib/auth/utils";
-import Link from "next/link";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import Link from "next/link"
 
-import { ModeToggle } from "@/components/ui/ThemeToggle";
+import {getUserAuth} from "@/lib/auth/utils"
+import {Avatar, AvatarFallback} from "@/components/ui/avatar"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import {ModeToggle} from "@/components/ui/ThemeToggle"
+import {Cpu} from "lucide-react";
 
 export default async function Navbar() {
-  const { session } = await getUserAuth();
-  const nameExists =
-    !!session?.user.name &&
-    session?.user.name.length > 5;
+    const {session} = await getUserAuth()
+    const nameExists =
+        !!session?.user.username && session?.user.username.length > 5
 
-
-  if (session?.user) {
-    return (
-      <nav className="py-2 flex items-center justify-between transition-all duration-300">
-        <h1 className="font-semibold hover:opacity-75 transition-hover cursor-pointer">
-          <Link href="/">Logo</Link>
-        </h1>
-        <div className="space-x-2 flex items-center">
-          <ModeToggle />
-          {session ? (
-            <DropdownMenu>
-              <DropdownMenuTrigger>
-                <Avatar>
-                  <AvatarFallback>
-                    {nameExists
-                      ? session.user.name
-                          ?.split(" ")
-                          .map((word) => word[0].toUpperCase())
-                          .join("")
-                      : "~"}
-                  </AvatarFallback>
-                </Avatar>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align="end" className="w-56">
-                <DropdownMenuLabel>
+    if (session?.user) {
+        return (
+            <nav className="flex items-center justify-between py-2 transition-all duration-300">
+                <h1 className="transition-hover cursor-pointer font-semibold text-2xl hover:opacity-75">
+                    <Link href="/" className={"flex flex-row items-center"}>
+                        <Cpu color={"rgb(185, 28, 28)"} className={"mr-1"}/>
+                        <span className={"font-extrabold text-red-700"}>TECH</span>
+                        pass
+                    </Link>
+                </h1>
+                <div className="flex items-center space-x-2">
+                    <ModeToggle/>
+                    {session ? (
+                        <DropdownMenu>
+                            <DropdownMenuTrigger>
+                                <Avatar>
+                                    <AvatarFallback>
+                                        {nameExists
+                                            ? session.user.username
+                                                ?.split(" ")
+                                                .map((word) => word[0].toUpperCase())
+                                                .join("")
+                                            : "~"}
+                                    </AvatarFallback>
+                                </Avatar>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent align="end" className="w-56">
+                                <DropdownMenuLabel>
                   <span className="font-semibold">
-                    {nameExists ? session.user.name : "New User"}
+                    {nameExists ? session.user.username : "New User"}
                   </span>
-                </DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <Link href="/account">
-                  <DropdownMenuItem className="cursor-pointer">
-                    Account
-                  </DropdownMenuItem>
-                </Link>
-                <Link href="/api/auth/signout">
-                  <DropdownMenuItem className="cursor-pointer">
-                    Sign out
-                  </DropdownMenuItem>
-                </Link>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          ) : (
-            <Link href="/sign-in">Sign in</Link>
-          )}
-
-        </div>
-      </nav>
-    );
-  } else return null;
+                                </DropdownMenuLabel>
+                                <DropdownMenuSeparator/>
+                                <Link href="/account">
+                                    <DropdownMenuItem className="cursor-pointer">
+                                        Account
+                                    </DropdownMenuItem>
+                                </Link>
+                                <Link href="/api/auth/signout">
+                                    <DropdownMenuItem className="cursor-pointer">
+                                        Sign out
+                                    </DropdownMenuItem>
+                                </Link>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    ) : (
+                        <Link href="/sign-in">Sign in</Link>
+                    )}
+                </div>
+            </nav>
+        )
+    } else return null
 }
