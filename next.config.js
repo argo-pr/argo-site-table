ed62fca1fb3c1fdd520f5ebad9bdf84569c99b24
Zeit241 <easywin.ru@gmail.com>
2023-10-29 21:11:35 +0300

    Незначительные фиксы

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
+    experimental: {
+        serverActions: true,
+    },
+    webpack: (config) => {
+        config.resolve.alias.canvas = false;
+        return config
+    }
 }
 
 module.exports = nextConfig
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,49 +1,53 @@
-import { NextResponse } from "next/server"
-import { getToken } from "next-auth/jwt"
-import { withAuth } from "next-auth/middleware"
+import {NextResponse} from "next/server"
+import {getToken} from "next-auth/jwt"
+import {withAuth} from "next-auth/middleware"
 
 export default withAuth(
-  async function middleware(req) {
-    const token = await getToken({ req })
+    async function middleware(req) {
+        const token = await getToken({req})
 
-    const isAuth = !!token
-    const isAuthPage = req.nextUrl.pathname.startsWith("/signin")
-    const isAdminPage = req.nextUrl.pathname.startsWith("/admin")
+        const isAuth = !!token
+        const isAuthPage = req.nextUrl.pathname.startsWith("/signin")
+        const isAdminPage = req.nextUrl.pathname.startsWith("/admin")
 
-    if (isAuthPage) {
-      if (isAuth) {
-        return NextResponse.redirect(new URL("/dashboard", req.url))
-      }
-      return null
-    }
+        if (isAuthPage) {
+            if (isAuth) {
+                return NextResponse.redirect(new URL("/dashboard", req.url))
+            }
+            return null
+        }
 
-    if (!isAuth) {
-      let from = req.nextUrl.pathname
-      if (req.nextUrl.search) {
-        from += req.nextUrl.search
-      }
 
-      return NextResponse.redirect(
-        new URL(`/login?from=${encodeURIComponent(from)}`, req.url)
-      )
-    } else {
-      if (isAdminPage && token?.user.role !== "ADMIN") {
-        return NextResponse.redirect(new URL(`/`, req.url))
-      }
-    }
-  },
-  {
-    callbacks: {
-      async authorized() {
-        // This is a work-around for handling redirect on auth pages.
-        // We return true here so that the middleware function above
-        // is always called.
-        return true
-      },
+        if (!isAuth) {
+            let from = req.nextUrl.pathname
+            if (req.nextUrl.search) {
+                from += req.nextUrl.search
+            }
+
+            return NextResponse.redirect(
+                new URL(`/signin?from=${encodeURIComponent(from)}`, req.url)
+            )
+        } else {
+            if (req.nextUrl.pathname === "/") {
+                return NextResponse.redirect(new URL("/dashboard", req.url))
+            }
+            if (isAdminPage && token?.user.role !== "ADMIN") {
+                return NextResponse.redirect(new URL(`/`, req.url))
+            }
+        }
     },
-  }
+    {
+        callbacks: {
+            async authorized() {
+                // This is a work-around for handling redirect on auth pages.
+                // We return true here so that the middleware function above
+                // is always called.
+                return true
+            },
+        },
+    }
 )
 
 export const config = {
-  matcher: ["/dashboard/:path*", "/signin", "/admin/:path*", "/profile:path*"],
+    matcher: ["/dashboard/:path*", "/signin", "/admin/:path*", "/profile:path*", "/"],
 }
