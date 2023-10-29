46719e2c378f55ee6ac88cd7d2e5754760caf245
Zeit241 <easywin.ru@gmail.com>
2023-10-29 21:15:51 +0300

    Роут с раздачей пдф'a

---
+++ src/app/api/order/[slug]/route.ts
@@ -0,0 +1,17 @@
+import generatePDF from "@/lib/pdf/pdf";
+import {NextApiResponse} from "next";
+import {NextRequest} from "next/server";
+
+async function handler(req: NextRequest, res: NextApiResponse) {
+    const download = req.nextUrl.searchParams?.get("download");
+    const pdfBytes = await generatePDF()
+    return new Response(pdfBytes, {
+        headers: {
+            'Content-Type': 'application/pdf',
+            'Content-Disposition': download ? 'attachment; filename=order.pdf' : "inline; filename=order.pdf"
+        },
+
+    });
+}
+
+export {handler as GET, handler as POST}
