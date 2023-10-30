20d6b46bc18c96511b816fea3e4a80f3d26dab13
Zeit241 <easywin.ru@gmail.com>
2023-10-30 20:43:15 +0300

    фикс

--- src/lib/pdf/pdf.ts
+++ src/lib/pdf/pdf.ts
@@ -1,19 +1,19 @@
-import {PDFDocument, StandardFonts, rgb} from 'pdf-lib'
+import {PDFDocument, StandardFonts} from 'pdf-lib'
 import fontkit from '@pdf-lib/fontkit';
 
 
 const generatePDF = async () => {
 
 // Embed the Times Roman font
-    const ubuntuFontBytes = await fetch(
-        'http://localhost:3000/arial.ttf',
-    ).then((res) => res.arrayBuffer());
+//     const ubuntuFontBytes = await fetch(
+//         '/arial.ttf',
+//     ).then((res) => res.arrayBuffer());
 
     const pdfDoc = await PDFDocument.create();
 
     pdfDoc.registerFontkit(fontkit);
 
-    const ubuntuFont = await pdfDoc.embedFont(ubuntuFontBytes);
+    const ubuntuFont = await pdfDoc.embedFont(StandardFonts.TimesRoman);
     const page = pdfDoc.addPage()
 
 // Get the width and height of the page
@@ -21,7 +21,7 @@ const generatePDF = async () => {
 
 // Draw a string of text toward the top of the page
     const fontSize = 30
-    page.drawText('ТУТ БУДЕТ PDF КОНТЕНТ', {x: 60, y: 500, size: 40, font: ubuntuFont});
+    page.drawText('PDF', {x: 60, y: 500, size: 40, font: ubuntuFont});
 
 // Serialize the PDFDocument to bytes (a Uint8Array)
     const pdfBytes = await pdfDoc.save({})
