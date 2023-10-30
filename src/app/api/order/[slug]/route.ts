f6889bf8976388283c1f02d7bfbc91cd4b2d6a65
Zeit241 <easywin.ru@gmail.com>
2023-10-30 20:38:30 +0300

    фикс

--- src/app/api/order/[slug]/route.ts
+++ src/app/api/order/[slug]/route.ts
@@ -1,29 +1,18 @@
-// import generatePDF from "@/lib/pdf/pdf";
-// import {NextApiResponse} from "next";
-// import {NextRequest} from "next/server";
-//
-// async function handler(req: NextRequest, res: NextApiResponse) {
-//     const download = req.nextUrl.searchParams?.get("download");
-//     const pdfBytes = await generatePDF()
-//     return new Response(pdfBytes, {
-//         headers: {
-//             'Content-Type': 'application/pdf',
-//             'Content-Disposition': download ? 'attachment; filename=order.pdf' : "inline; filename=order.pdf"
-//         },
-//
-//     });
-// }
-//
-// export {handler as GET, handler as POST}\
+import generatePDF from "@/lib/pdf/pdf";
+import {NextApiResponse} from "next";
+import {NextRequest} from "next/server";
 
-import {headers} from 'next/headers'
+async function handler(req: NextRequest, res: NextApiResponse) {
+    const download = req.nextUrl.searchParams?.get("download");
+    const pdfBytes = await generatePDF()
+    return new Response(pdfBytes, {
+        headers: {
+            'Content-Type': 'application/pdf',
+            'Content-Disposition': download ? 'attachment; filename=order.pdf' : "inline; filename=order.pdf"
+        },
 
-export async function GET(request: Request) {
-    const headersList = headers()
-    const referer = headersList.get('referer')
-
-    return new Response('Hello, Next.js!', {
-        status: 200,
-        //headers: {referer: referer},
-    })
+    });
 }
+
+export {handler as GET}
+
