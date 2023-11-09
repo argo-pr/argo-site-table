e1d032d65cc8725bfea6ef0cd4001415916ceac6
Zeit241 <easywin.ru@gmail.com>
2023-11-09 16:54:59 +0300

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,10 @@
-import SignIn from "@/components/auth/SignIn";
 import {getUserAuth} from "@/lib/auth/utils";
 import CreateUserCard from "@/components/CreateUserCard";
 
 export default async function Home() {
     const {session} = await getUserAuth();
     return (
-        <main className="space-y-4 pt-2">
-            {session ? (
-                <>
-                    <CreateUserCard/>
-                </>
-
-
-            ) : null}
-            <SignIn/>
-        </main>
+        <>
+        </>
     );
 }
